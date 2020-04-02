cc2f9dd5a2a372491270e668f1b7446963614169
FernandaHamada <fernandakubayashi@gmail.com>
2020-04-02 11:45:05 -0300

    Session

---
+++ CaelumEstoque/Controllers/LoginController.cs
@@ -0,0 +1,34 @@
+﻿using CaelumEstoque.DAO;
+using CaelumEstoque.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CaelumEstoque.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Autentica(String login, String senha)
+        {
+            UsuariosDAO dao = new UsuariosDAO();
+            Usuario usuario = dao.Busca(login, senha);
+            // usuário existente
+            if (usuario != null){
+                Session["usuarioLogado"] = usuario;
+                return RedirectToAction("Index", "Produto"); // retorna para Lista de produtos
+            }
+            else
+            {
+                return RedirectToAction("Index"); // caso não exista, retorna para a página de login novamente
+            }
+        }
+    }
+}
